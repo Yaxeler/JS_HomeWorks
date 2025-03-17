6e6b8401173f8f5b4f5e05a533a8ff8529d34f4c
Yaxeler <yaxeler@gmail.com>
2025-03-17 16:52:34 +0300

    Загрузил дз_1 и дз_2 в репозиторий

---
+++ HW1/index.js
@@ -0,0 +1,73 @@
+console.log ("Hello World!");
+
+const age = 22;
+
+let userName = 'Alex';
+
+let isPayment = true;
+
+let aVoid = null;
+
+let userType;
+
+const person = {
+    name: 'Oleg',
+    age: 30
+};
+
+let mySymbol = Symbol ('description');
+
+let bigNumber = 21321312312312n;
+
+console.log (age);
+
+console.log (userName);
+
+console.log (isPayment);
+
+console.log (aVoid);
+
+console.log (userType);
+
+console.log (person.name);
+
+console.log (mySymbol);
+
+console.log (bigNumber);
+
+console.log (typeof age);
+
+console.log (typeof userName);
+
+console.log (typeof isPayment);
+
+console.log (typeof aVoid);
+
+console.log (typeof userType);
+
+console.log (typeof person);
+
+console.log (typeof mySymbol);
+
+console.log (typeof bigNumber);
+
+// age = 20; изменение переменной const
+
+person.name = 'Andrew';
+console.log (person.name);
+
+let obj = { name: "Alice" };
+
+obj.name = "Bob"; 
+console.log(obj.name);
+
+obj = { name: "Charlie" };
+console.log(obj.name);
+
+var clientDate = { name: "Jake" };
+
+clientDate.name = "Robin";
+console.log(clientDate.name);
+
+clientDate = { name: "Jaze" };
+console.log(clientDate.name);
